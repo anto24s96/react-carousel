334fda3c0b1b741d9c1731bcb05211a2639a1cfe
anto24s96 <anto24s96@gmail.com>
2024-10-16 17:45:13 +0200

    created slides and imported images

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Carousel from "./components/Carousel";
+import image1 from "./assets/Emirates_stadium.jpg";
+import image2 from "./assets/anfield.jpg";
+import image3 from "./assets/etihad_stadium.jpg";
+import image4 from "./assets/old_trafford.jpg";
+import image5 from "./assets/stanford_bridge.jpg";
+import image6 from "./assets/tottenham.jpg";
 
-function App() {
-  const [count, setCount] = useState(0)
+const slides = [
+    {
+        image: image1,
+        title: "Slide 1",
+    },
+    {
+        image: image2,
+        title: "Slide 2",
+    },
+    {
+        image: image3,
+        title: "Slide 3",
+    },
+    {
+        image: image4,
+        title: "Slide 4",
+    },
+    {
+        image: image5,
+        title: "Slide 5",
+    },
+    {
+        image: image6,
+        title: "Slide 6",
+    },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    return (
+        <>
+            <Carousel />
+        </>
+    );
+};
 
-export default App
+export default App;
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,5 @@
+const Carousel = () => {
+    return <div>carosello</div>;
+};
+
+export default Carousel;
